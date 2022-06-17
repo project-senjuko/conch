3bf6c8628879e9c241402dbd0332cef252a31fc8
千橘 雫霞 <i@qianjunakasumi.ren>
2022-06-17 14:11:32 +0800

    pref📄 版权通知

--- src/cookie/network/protocol/frame/jce/field/short.rs
+++ src/cookie/network/protocol/frame/jce/field/short.rs
@@ -1,3 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 qianjunakasumi <i@qianjunakasumi.ren>                    /
+//                     qianjunakasumi <qianjunakasumi@outlook.com>             /
+//                     https://github.com/qianjunakasumi                       /
+//                                                                             /
+//     This Source Code Form is subject to the terms of the Mozilla Public     /
+//     License, v. 2.0. If a copy of the MPL was not distributed with this     /
+//     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
+////////////////////////////////////////////////////////////////////////////////
+
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 
 use super::{BYTE, HeadData, JceType, JShort, SHORT, TYPE_ERR, ZERO_TAG};
