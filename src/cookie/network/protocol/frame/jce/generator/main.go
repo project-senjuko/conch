9290fe4170c292a2705e4a03e86a5471897d1c11
千橘 雫霞 <i@qianjunakasumi.ren>
2022-06-20 23:33:48 +0800

    feat✨cookie-jce 遍历读取 Jce 定义文件

--- src/cookie/network/protocol/frame/jce/generator/main.go
+++ src/cookie/network/protocol/frame/jce/generator/main.go
@@ -10,5 +10,59 @@
 
 package main
 
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"sync"
+)
+
+var Ver *VersionSpec
+
 func main() {
+	fmt.Println("少女祈祷中...")
+
+	con := ReadConfigSpec()
+	dir := filepath.Dir(con.Spec.Source)
+	Ver = ReadVersionSpec(filepath.Join(dir, "version.yml"))
+	wg := sync.WaitGroup{}
+
+	err := filepath.WalkDir(filepath.Join(dir, "struct"), func(p string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Println("警告[遍历途中] | " + err.Error())
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+
+		wg.Add(1)
+		go read(p, &wg) // 考虑协程池
+		return nil
+	})
+	if err != nil {
+		fmt.Println("警告[遍历结束] | " + err.Error())
+	}
+	wg.Wait()
+
+	// TODO 统计信息
+
+	fmt.Println("Done.")
+}
+
+func read(p string, wg *sync.WaitGroup) {
+	j := ReadJceSpec(p)
+	if j.Metadata.UpstreamVersion > Ver.Spec.Current {
+		panic("请检查版本文件是否未更新：upstreamVersion > current")
+	}
+	if j.Metadata.UpstreamVersion < Ver.Spec.Minimal {
+		panic("Jce 版本滞后于最低容忍版本，请更新 Jce")
+	}
+	if j.Metadata.UpstreamVersion != Ver.Spec.Current {
+		fmt.Println("信息[Jce] | 请注意更新 " + p)
+	}
+
+	// TODO 生成 rust 文件
+
+	wg.Done()
 }
