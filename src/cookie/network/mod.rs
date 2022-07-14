636f1c446481ba48e4f7e20ac7788b2bc32a5c44
千橘 雫霞 <i@qianjunakasumi.ren>
2022-07-14 12:44:22 +0800

    feat✨ 包公开化

--- src/cookie/network/mod.rs
+++ src/cookie/network/mod.rs
@@ -8,4 +8,4 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
-pub mod jce;
+pub mod protocol;
--- src/cookie/network/protocol/mod.rs
+++ src/cookie/network/protocol/mod.rs
@@ -8,4 +8,4 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
-mod r#struct;
+pub mod r#struct;
