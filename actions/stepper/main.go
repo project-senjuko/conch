ff28930429b504e947ed783948f787512c3c7d41
千橘 雫霞 <i@qianjunakasumi.ren>
2022-07-24 17:49:13 +0800

    feat✨actions-stepper 触发器和步进执行器

---
+++ actions/stepper/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type VersionConf struct {
+	version string
+	code    uint64
+	appId   uint64
+}
+
+type Arm func(oldVersion *VersionConf, newVersion *VersionConf) error
+
+var arm = []Arm{}
+
+func main() {
+	v := readVersionConf()
+
+	body := requestHTML()
+	code, appId := parseDownloadURL(readDownloadURL(body))
+	nv := VersionConf{
+		version: readVersion(body),
+		code:    code,
+		appId:   appId,
+	}
+
+	if code <= v.code {
+		fmt.Println("== 版本已同步，无需更新 ==")
+		return
+	}
+	fmt.Println("== 开始更新 ==")
+
+	for _, a := range arm {
+		err := a(v, &nv)
+		if err != nil {
+			fmt.Println("[ERR] ", err)
+		}
+	}
+
+	fmt.Println("== 更新完成 ==")
+}
+
+func readVersionConf() *VersionConf {
+	f, err := os.OpenFile("../../VERSION.yml", os.O_RDONLY, 0444)
+	if err != nil {
+		panic("打开 VERSION.yml 文件失败：" + err.Error())
+	}
+	defer f.Close()
+	fb, err := io.ReadAll(f)
+	if err != nil {
+		panic("读取 VERSION.yml 文件失败：" + err.Error())
+	}
+
+	a := VersionConf{}
+	if err := yaml.NewDecoder(bytes.NewReader(fb)).Decode(&a); err != nil {
+		panic("解析 VERSION.yml 文件失败：通用 Spec 解析失败" + err.Error())
+	}
+	return &a
+}
