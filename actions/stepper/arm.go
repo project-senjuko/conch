a8cdc1b640163acb7398872071b4cf8050b3a052
千橘 雫霞 <i@qianjunakasumi.ren>
2022-07-25 13:02:29 +0800

    pref♻️actions-stepper 复用代码并减小复杂度

--- actions/stepper/arm.go
+++ actions/stepper/arm.go
@@ -13,18 +13,15 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"path/filepath"
-	"strconv"
-	"strings"
 	"sync"
 
 	"gopkg.in/yaml.v3"
 )
 
-func updateVERSION(_ *VersionConf, n *VersionConf) (err error) {
+func updateVERSION(_, n *VersionConf) (err error) {
 	const d = "VERSION.yml"
 	fmt.Println("正在更新 " + d)
 
@@ -41,43 +38,18 @@ func updateVERSION(_ *VersionConf, n *VersionConf) (err error) {
 	return
 }
 
-func updateJceVersion(o *VersionConf, n *VersionConf) (err error) {
+func updateJceVersion(o, n *VersionConf) (err error) {
 	const d = "jce version.yml"
 	fmt.Println("正在更新 " + d)
 
-	const path = "../../crates/cookie/src/network/protocol/struct/jce/version.yml"
-	f, err := os.OpenFile(path, os.O_RDONLY, 0444)
-	if err != nil {
-		return errors.New("打开 " + d + " 读取流失败：" + err.Error())
-	}
-	defer f.Close()
-
-	fb, err := io.ReadAll(f)
-	if err != nil {
-		return errors.New("读取 " + d + " 失败：" + err.Error())
-	}
-
-	f, err = os.OpenFile(path, os.O_WRONLY|os.O_TRUNC, 0200)
-	if err != nil {
-		return errors.New("打开 " + d + " 写入流失败：" + err.Error())
-	}
-	defer f.Close()
-
-	_, err = f.WriteString(
-		strings.Replace(
-			string(fb),
-			"  current: "+strconv.FormatUint(o.Code, 10),
-			"  current: "+strconv.FormatUint(n.Code, 10),
-			1,
-		),
+	return ReadReplaceAndWrite(
+		"../../crates/cookie/src/network/protocol/struct/jce/version.yml",
+		d,
+		ReplaceCode(o, n, "  current: "),
 	)
-	if err != nil {
-		return errors.New("写入 " + d + " 失败：" + err.Error())
-	}
-	return
 }
 
-func updateJceStructVersion(o *VersionConf, n *VersionConf) (err error) {
+func updateJceStructVersion(o, n *VersionConf) (err error) {
 	wg := sync.WaitGroup{}
 	err = filepath.WalkDir(
 		"../../crates/cookie/src/network/protocol/struct/jce/struct",
@@ -102,36 +74,12 @@ func updateJceStructVersion(o *VersionConf, n *VersionConf) (err error) {
 	return
 }
 
-func _updateJceStructVersion(p string, o *VersionConf, n *VersionConf, wg *sync.WaitGroup) {
+func _updateJceStructVersion(p string, o, n *VersionConf, wg *sync.WaitGroup) {
 	fmt.Println("正在更新 " + p)
 
-	f, err := os.OpenFile(p, os.O_RDONLY, 0444)
+	err := ReadReplaceAndWrite(p, p, ReplaceCode(o, n, "  upstreamVersion: "))
 	if err != nil {
-		fmt.Println("打开 "+p+" 读取流失败：", err.Error())
-	}
-	defer f.Close()
-
-	fb, err := io.ReadAll(f)
-	if err != nil {
-		fmt.Println("读取 "+p+" 失败：", err.Error())
-	}
-
-	f, err = os.OpenFile(p, os.O_WRONLY|os.O_TRUNC, 0200)
-	if err != nil {
-		fmt.Println("打开 "+p+" 写入流失败：", err.Error())
-	}
-	defer f.Close()
-
-	_, err = f.WriteString(
-		strings.Replace(
-			string(fb),
-			"  upstreamVersion: "+strconv.FormatUint(o.Code, 10),
-			"  upstreamVersion: "+strconv.FormatUint(n.Code, 10),
-			1,
-		),
-	)
-	if err != nil {
-		fmt.Println("写入 "+p+" 失败：", err.Error())
+		fmt.Println(err)
 	}
 	wg.Done()
 }
---
+++ actions/stepper/utils.go
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022 qianjunakasumi <i@qianjunakasumi.ren>                    /
+//                     qianjunakasumi <qianjunakasumi@outlook.com>             /
+//                     https://github.com/qianjunakasumi                       /
+//                                                                             /
+//     This Source Code Form is subject to the terms of the Mozilla Public     /
+//     License, v. 2.0. If a copy of the MPL was not distributed with this     /
+//     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
+////////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"errors"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadReplaceAndWrite(p, d string, rp ...[2]string) (err error) {
+	f, err := os.OpenFile(p, os.O_RDONLY, 0444)
+	if err != nil {
+		return errors.New("打开 " + d + " 读取流失败：" + err.Error())
+	}
+	defer f.Close()
+
+	fb, err := io.ReadAll(f)
+	if err != nil {
+		return errors.New("读取 " + d + " 失败：" + err.Error())
+	}
+
+	f, err = os.OpenFile(p, os.O_WRONLY|os.O_TRUNC, 0200)
+	if err != nil {
+		return errors.New("打开 " + d + " 写入流失败：" + err.Error())
+	}
+	defer f.Close()
+
+	b := string(fb)
+	for _, v := range rp {
+		b = strings.Replace(b, v[0], v[1], 1)
+	}
+
+	_, err = f.WriteString(b)
+	if err != nil {
+		return errors.New("写入 " + d + " 失败：" + err.Error())
+	}
+	return
+}
+
+func ReplaceCode(o, n *VersionConf, prefix string) [2]string {
+	return [2]string{
+		prefix + strconv.FormatUint(o.Code, 10),
+		prefix + strconv.FormatUint(n.Code, 10),
+	}
+}
