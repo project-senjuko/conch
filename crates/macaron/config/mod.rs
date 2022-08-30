cfcec86113b07ced94e542f4af95fde4f08b54e5
千橘 雫霞 <i@qianjunakasumi.ren>
2022-08-30 16:17:26 +0800

    feat✨conch-macaron 加载配置文件

--- crates/macaron/config/mod.rs
+++ crates/macaron/config/mod.rs
@@ -8,4 +8,30 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
+use std::fs::read;
+
+use anyhow::Result;
+use tracing::{error, instrument};
+
+use r#struct::Config;
+
 mod r#struct;
+
+/// 加载配置文件
+#[instrument]
+fn load_config(p: String) -> Result<Config> {
+    let b = read(&p);
+    if b.is_err() {
+        error!(
+            dsc = "读取配置文件失败", path = p,
+            err = %b.as_ref().unwrap_err(),
+        );
+    }
+
+    let c = toml::from_slice(&*b?);
+    if c.is_err() {
+        error!(dsc = "解析配置文件失败", err = %c.as_ref().unwrap_err())
+    }
+
+    Ok(c?)
+}
