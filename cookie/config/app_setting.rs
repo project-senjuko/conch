f4dd20e449446800837c4f56114b01d05545293b
qianjunakasumi <qianjunakasumi@users.noreply.github.com>
2022-09-22 03:42:11 +0000

    pref👽️version 同步至上游版本
    Signed-off-by: GitHub <noreply@github.com>

--- cookie/config/app_setting.rs
+++ cookie/config/app_setting.rs
@@ -8,6 +8,6 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
-/// APP_ID: 版本 | 9135
+/// APP_ID: 版本 | 9145
 /// 源 | com.tencent.common.config.AppSetting.f
-pub const APP_ID: u32 = 537135523;
+pub const APP_ID: u32 = 537135907;
