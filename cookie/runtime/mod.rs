05d67c8366892d614864bbcc7352cabb3f4b660a
千橘 雫霞 <i@qianjunakasumi.ren>
2022-12-15 19:24:57 +0800

    feat✨cookie-runtime `d2`、`tgt`、`msg_cookie`

--- cookie/runtime/mod.rs
+++ cookie/runtime/mod.rs
@@ -8,6 +8,8 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
+use bytes::Bytes;
+
 use self::structs::Config;
 use self::types::*;
 
@@ -21,7 +23,10 @@ static mut RUNTIMEVAR: Option<&mut Runtime> = None;
 /// 全局运行时
 pub struct Runtime {
     config: Config,
+    d2: Bytes,
     d2key: D2Key,
+    tgt: Bytes,
+    msg_cookie: Bytes,
 
     // 计数器等
 }
@@ -33,7 +38,10 @@ impl Runtime {
             RUNTIMEVAR = Some(Box::leak(Box::new(
                 Runtime {
                     config: Config::read_config(),
+                    d2: Default::default(),
                     d2key: Default::default(),
+                    tgt: Default::default(),
+                    msg_cookie: Default::default(),
                 }
             )));
         }
@@ -55,9 +63,32 @@ impl Runtime {
 }
 
 impl Runtime {
+    /// 获取配置文件
+    pub fn get_config() -> &'static Config { &Runtime::get_var().config }
+
+    /// 获取 d2
+    pub fn get_d2() -> &Bytes { &Runtime::get_var().d2 }
+
     /// 获取 d2key
     pub fn get_d2key() -> D2Key { Runtime::get_var().d2key }
 
-    /// 获取配置文件
-    pub fn get_config() -> &'static Config { &Runtime::get_var().config }
+    /// 获取 tgt
+    pub fn get_tgt() -> &Bytes { &Runtime::get_var().tgt }
+
+    /// 获取 msg_cookie
+    pub fn get_msg_cookie() -> &Bytes { &Runtime::get_var().msg_cookie }
+}
+
+impl Runtime {
+    /// 设置 d2
+    pub fn put_d2(b: Bytes) { Runtime::get_var_mut().d2 = b }
+
+    /// 设置 d2key
+    pub fn put_d2key(d: D2Key) { Runtime::get_var_mut().d2key = d }
+
+    /// 设置 tgt
+    pub fn put_tgt(b: Bytes) { Runtime::get_var_mut().tgt = b }
+
+    /// 设置 msg_cookie
+    pub fn put_msg_cookie(b: Bytes) { Runtime::get_var_mut().msg_cookie = b }
 }
