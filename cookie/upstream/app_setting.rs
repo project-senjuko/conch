277131ccf1807d27373ea46ec38f4ac4e4c15cff
qianjunakasumi <qianjunakasumi@users.noreply.github.com>
2023-01-12 02:18:08 +0000

    pref👽️version 同步至上游版本
    Signed-off-by: GitHub <noreply@github.com>

--- cookie/upstream/app_setting.rs
+++ cookie/upstream/app_setting.rs
@@ -8,9 +8,9 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
-/// APP_ID: 版本 | 10150
+/// APP_ID: 版本 | 10155
 /// 源 | com.tencent.common.config.AppSetting.f
-pub const APP_ID: u32 = 537147234;
+pub const APP_ID: u32 = 537147618;
 
 pub const APP_SHORT_VER: &str = "8.9.28";
 
