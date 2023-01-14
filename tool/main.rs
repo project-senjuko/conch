f401e0c0ec12ac801d57c3d7b848425ac6cc318d
StarWishsama <starwishsama@outlook.com>
2023-01-14 18:08:43 +0800

    feat✨tool 引入 `clap`

--- tool/main.rs
+++ tool/main.rs
@@ -8,6 +8,29 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
+use clap::{Parser, ValueHint};
+use std::env;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// 输入文件路径
+    #[clap(short = 'i', long = "input")]
+    #[clap(value_name = "INPUT")]
+    #[arg(value_hint = ValueHint::FilePath)]
+    input: PathBuf,
+    /// 输出文件路径
+    #[clap(short = 'o', long = "output")]
+    #[clap(value_name = "OUTPUT")]
+    #[arg(value_hint = ValueHint::FilePath)]
+    output: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, World!")
+    let cli = Cli::parse();
+
+    let work_dir = env::current_dir().expect("Failed to get current workdir");
+    let work_dir = cli.work_dir.unwrap_or(work_dir);
+    println!("当前工作目录为 {}", &work_dir.to_string_lossy())
 }
