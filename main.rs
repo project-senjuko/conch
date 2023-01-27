909511961687895aa438a397a95a1e28aba5d3f4
千橘 雫霞 <i@qianjunakasumi.ren>
2023-01-27 14:19:28 +0800

    pref♻️conch 从文件中抽离并合并子组件于 `main`
    Signed-off-by: qianjunakasumi <i@qianjunakasumi.ren>

--- main.rs
+++ main.rs
@@ -8,19 +8,18 @@
 //     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
 ////////////////////////////////////////////////////////////////////////////////
 
-use std::time::Duration;
-
-use anyhow::Result;
-use shadow_rs::shadow;
-use tokio::{spawn, time::sleep};
-use tracing::{info, instrument};
-
-use cookie::runtime::Runtime;
-
-use self::{http::dashboard, logger::init_logger};
-
-mod http;
-mod logger;
+use {
+    anyhow::Result,
+    axum::{http::StatusCode, response::IntoResponse, Router, routing::{get, get_service}},
+    axum_extra::routing::SpaRouter,
+    cookie::runtime::Runtime,
+    shadow_rs::shadow,
+    std::{io::Error, net::SocketAddr, time::Duration},
+    tokio::{spawn, time::sleep},
+    tower_http::services::ServeFile,
+    tracing::{info, instrument},
+    tracing_subscriber::{EnvFilter, filter, fmt, layer, prelude::*, Registry, reload},
+};
 
 shadow!(build);
 
@@ -68,6 +67,54 @@ async fn main() -> Result<()> {
     // 通知停机
     info!(dsc = "プログラムは停止しますた、次回をお楽しみなのじゃ");
     Ok(())
+}
+
+/// 启动 Dashboard 服务
+pub async fn dashboard() {
+    let app = Router::new()
+        .route(
+            "/conch-cgi/hello",
+            get(|| async { "Conch 海螺 Dashboard 服务已正确运行" }),
+        )
+        .route(
+            "/favicon.svg",
+            get_service(ServeFile::new("dashboard/favicon.svg"))
+                .handle_error(handle_error),
+        )
+        .merge(
+            SpaRouter::new("/assets", "dashboard/assets")
+                .index_file("../index.html")
+        );
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 1919));
+    info!(dsc = "Dashboard 服务启用", addr = %addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handle_error(err: Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
+
+/// 日志记录器初始化
+#[instrument]
+pub fn init_logger() -> (String, reload::Handle<EnvFilter, layer::Layered<fmt::Layer<Registry>, Registry>>) {
+    let e = EnvFilter::builder()
+        .with_default_directive(filter::LevelFilter::INFO.into())
+        .with_env_var("SJKCONCH_LOG")
+        .from_env_lossy();
+    let lev = e.to_string();
+    let (lay, h) = reload::Layer::new(e);
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(lay)
+        .init();
+
+    (lev, h)
 }
 
 #[cfg(unix)]
