8fba70eaf80b0d212a2257ecd46dec789beecddc
千橘 雫霞 <i@qianjunakasumi.ren>
2023-02-07 10:33:10 +0800

    feat✨oicq t109
    Signed-off-by: 千橘 雫霞 <i@qianjunakasumi.ren>

---
+++ src/network/protocol/oicq/tlv/t109.rs
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2022-2023 qianjunakasumi <i@qianjunakasumi.ren>               /
+//                     qianjunakasumi <qianjunakasumi@outlook.com>             /
+//                     https://github.com/qianjunakasumi                       /
+//                                                                             /
+//     This Source Code Form is subject to the terms of the Mozilla Public     /
+//     License, v. 2.0. If a copy of the MPL was not distributed with this     /
+//     file, You can obtain one at http://mozilla.org/MPL/2.0/.                /
+////////////////////////////////////////////////////////////////////////////////
+
+use {
+    bytes::BytesMut,
+    crate::runtime::Runtime,
+    super::TlvField,
+};
+
+struct TlvT109 {
+    android_id_md5: [u8; 16],
+}
+
+impl Default for TlvT109 {
+    fn default() -> Self {
+        Self {
+            android_id_md5: Runtime::secret().android_id_md5,
+        }
+    }
+}
+
+impl TlvField for TlvT109 {
+    fn tag() -> u16 { 0x109 }
+
+    fn to_payload(&self, b: &mut BytesMut) {
+        b.reserve(16);
+        b.extend_from_slice(&self.android_id_md5);
+    }
+}
