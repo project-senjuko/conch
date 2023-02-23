ff3661e18f126a2226fd5036b166ee01ff9ffb27
千橘 雫霞 <i@qianjunakasumi.ren>
2023-02-23 20:05:30 +0800

    feat✨oicq/tlv tlv124

--- src/network/protocol/oicq/tlv/t124.rs
+++ src/network/protocol/oicq/tlv/t124.rs
@@ -10,47 +10,44 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.                                      /
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+use {
+    bytes::{BufMut, BytesMut},
+    super::TlvField,
+    crate::utils::bytes::PutStrWith16Len,
+};
 
-// 0x144
-//    128  16e
-//  0x147, 0x154, 0x141
-// 0x187 ,0x188, 0x191, 0x177
-// 0x516, 0x521, 0x525, 0x544,0x545, 0x548, 0x542
+struct TlvT124 {
+    os_type: String,
+    os_version: String,
+    network_type: u16,
+    sim_operator_name: String,
+    wtf: String,
+    apn_string: String,
+}
 
+impl Default for TlvT124 {
+    fn default() -> Self {
+        Self {
+            os_type: String::from("android"),
+            os_version: String::from("14"), // need const
+            network_type: 2,
+            sim_operator_name: Default::default(),
+            wtf: Default::default(),
+            apn_string: String::from("wifi"),
+        }
+    }
+}
 
-use bytes::{BufMut, BytesMut};
+impl TlvField for TlvT124 {
+    fn tag() -> u16 { 0x124 }
 
-pub mod t1;
-pub mod t8;
-pub mod t18;
-pub mod t100;
-pub mod t106;
-pub mod t107;
-pub mod t109;
-pub mod t116;
-pub mod t124;
-pub mod t142;
-pub mod t145;
-pub mod t187;
-pub mod t188;
-pub mod t511;
-pub mod t52d;
-
-trait TlvField: Default {
-    fn tag() -> u16;
-
-    fn to_payload(&self, b: &mut BytesMut);
-
-    fn to_bytes(&self) -> BytesMut {
-        let mut b = BytesMut::with_capacity(4);
-
-        b.put_u16(Self::tag());
-        b.put_u16(0); // payload length
-        self.to_payload(&mut b);
-
-        let l = b.len() - 4;
-        b[2..4].swap_with_slice(&mut l.to_be_bytes()); // set payload length
-
-        b
+    fn to_payload(&self, b: &mut BytesMut) {
+        b.reserve(25);
+        b.put_string_with_16len(&self.os_type);
+        b.put_string_with_16len(&self.os_version);
+        b.put_u16(self.network_type);
+        b.put_string_with_16len(&self.sim_operator_name);
+        b.put_string_with_16len(&self.wtf);
+        b.put_string_with_16len(&self.apn_string);
     }
 }
