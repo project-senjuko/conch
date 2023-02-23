319c518e3c242469e65f2c242d844ce0ee53b9b0
千橘 雫霞 <i@qianjunakasumi.ren>
2023-02-23 20:02:11 +0800

    feat✨utils 写入头部为 16 位长度标识的字符串

--- src/utils/bytes/put_str_with_16len.rs
+++ src/utils/bytes/put_str_with_16len.rs
@@ -10,31 +10,16 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.                                      /
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-mod bytes2string;
-mod get_sized;
-mod put_str_with_16len;
+use {
+    bytes::{BufMut, BytesMut},
+    super::PutStrWith16Len,
+};
 
-/// 提供字节转换 utf8 字符串的方法
-pub trait Bytes2String {
-    /// 获取指定长度的 utf8 字符串
-    ///
-    /// # Safety
-    ///
-    /// 当指定长度 > remaining 时将 panic。
-    fn get_string(&mut self, len: usize) -> String;
-}
-
-/// 提供获取特定长度字节的方法
-pub trait GetSized {
-    /// 获取特定长度字节
-    ///
-    /// # Safety
-    ///
-    /// 当指定长度 > remaining 时将 panic。
-    fn get_sized(&mut self, len: usize) -> Self;
-}
-
-pub trait PutStrWith16Len {
-    /// 写入头部为 16 位长度标识的字符串
-    fn put_string_with_16len(&mut self, s: &str);
+impl PutStrWith16Len for BytesMut {
+    fn put_string_with_16len(&mut self, s: &str) {
+        let l = s.len();
+        self.reserve(l);
+        self.put_u16(l as u16);
+        self.extend(s.as_bytes());
+    }
 }
