7a12444de51d21ec07d2c50c04a6cbd5a6b4daee
千橘 雫霞 <i@qianjunakasumi.ren>
2023-02-24 22:18:20 +0800

    feat✨oicq/tlv t147

--- src/network/protocol/oicq/tlv/t147.rs
+++ src/network/protocol/oicq/tlv/t147.rs
@@ -10,45 +10,39 @@
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.                                      /
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// 0x154, 0x141 0x187 ,0x188, 0x191, 0x177 0x516, 0x521, 0x525, 0x544,0x545, 0x548, 0x542
+use {
+    bytes::{BytesMut, BufMut},
+    super::TlvField,
+    crate::{
+        common::upstream::{APP_SHORT_VER, APK_SIGNATURE_MD5},
+        utils::bytes::PutStrWith16Len,
+    },
+};
 
-use bytes::{BufMut, BytesMut};
+struct TlvT147 {
+    app_id: u32,
+    apk_v: String,
+    pkg_sig_from_apk_name: [u8; 16],
+}
 
-pub mod t1;
-pub mod t8;
-pub mod t18;
-pub mod t100;
-pub mod t106;
-pub mod t107;
-pub mod t109;
-pub mod t116;
-pub mod t124;
-pub mod t128;
-pub mod t142;
-pub mod t144;
-pub mod t145;
-pub mod t147;
-pub mod t16e;
-pub mod t187;
-pub mod t188;
-pub mod t511;
-pub mod t52d;
+impl Default for TlvT147 {
+    fn default() -> Self {
+        Self {
+            app_id: 16, // const need
+            apk_v: APP_SHORT_VER.to_string(),
+            pkg_sig_from_apk_name: APK_SIGNATURE_MD5,
+        }
+    }
+}
 
-trait TlvField: Default {
-    fn tag() -> u16;
+impl TlvField for TlvT147 {
+    fn tag() -> u16 { 0x147 }
 
-    fn to_payload(&self, b: &mut BytesMut);
-
-    fn to_bytes(&self) -> BytesMut {
-        let mut b = BytesMut::with_capacity(4);
-
-        b.put_u16(Self::tag());
-        b.put_u16(0); // payload length
-        self.to_payload(&mut b);
-
-        let l = b.len() - 4;
-        b[2..4].swap_with_slice(&mut l.to_be_bytes()); // set payload length
-
-        b
+    fn to_payload(&self, b: &mut BytesMut) {
+        b.reserve(0);
+        b.put_u32(self.app_id);
+        b.put_string_with_16len(&self.apk_v);
+        b.put_u16(16); // length
+        b.extend(self.pkg_sig_from_apk_name);
     }
 }
