f88c97f3b2b2f6018715d6cf9376909521588036
千橘 雫霞 <i@qianjunakasumi.moe>
2023-03-14 21:10:12 +0900

    feat⚡️oicq/tlv 线性写入 tlv

--- src/network/protocol/oicq/tlv/mod.rs
+++ src/network/protocol/oicq/tlv/mod.rs
@@ -12,7 +12,7 @@
 
 // 0x544 unknown 0x542
 
-use bytes::{BufMut, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 
 pub mod t1;
 pub mod t8;
@@ -48,16 +48,44 @@ trait TlvField: Default {
 
     fn to_payload(&self, b: &mut BytesMut);
 
+    fn put_bytes(&self, b: &mut BytesMut) {
+        b.reserve(4);
+        b.put_u16(Self::tag());
+        let lenp = b.remaining();
+
+        b.put_u16(0); // payload length
+        self.to_payload(b);
+
+        let l = (b.remaining() - lenp - 2) as u16;
+        b[lenp..lenp + 2].swap_with_slice(&mut l.to_be_bytes()); // set payload length
+    }
+
     fn to_bytes(&self) -> BytesMut {
         let mut b = BytesMut::with_capacity(4);
-
-        b.put_u16(Self::tag());
-        b.put_u16(0); // payload length
-        self.to_payload(&mut b);
-
-        let l = b.len() - 4;
-        b[2..4].swap_with_slice(&mut l.to_be_bytes()); // set payload length
-
+        self.put_bytes(&mut b);
         b
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        bytes::BytesMut,
+        super::{t1::TlvT1, TlvField},
+    };
+
+    const T1: TlvT1 = TlvT1 { ip_ver: 1, uin: 114514 };
+
+    #[test]
+    fn put_bytes() {
+        let mut a = BytesMut::new();
+        T1.put_bytes(&mut a);
+        T1.put_bytes(&mut a);
+        T1.put_bytes(&mut a);
+
+        println!("{:?}", a.to_vec());
+    }
+
+    #[test]
+    fn to_bytes() { println!("{:?}", T1.to_bytes().to_vec()); }
 }
