2437a22c1f63f696e8fea41656d26307e720c28b
千橘 雫霞 <i@qianjunakasumi.moe>
2023-03-22 21:08:47 +0900

    pref♻️oicq/tlv Optional(Disable) Default Trait
    Signed-off-by: 千橘 雫霞 <i@qianjunakasumi.moe>

--- src/network/protocol/oicq/tlvs/mod.rs
+++ src/network/protocol/oicq/tlvs/mod.rs
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2022-present qianjunakasumi <i@qianjunakasumi.ren>                                /
+// Copyright (c) 2022-present qianjunakasumi <i@qianjunakasumi.moe>                                /
 //                            project-senjuko/conch Contributors                                   /
 //                                                                                                 /
 //           https://github.com/qianjunakasumi                                                     /
@@ -8,6 +8,7 @@
 //   This Source Code Form is subject to the terms of the Mozilla Public                           /
 //   License, v. 2.0. If a copy of the MPL was not distributed with this                           /
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.                                      /
+//   More information at https://github.com/project-senjuko/conch.                                 /
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // 0x544 unknown 0x542
@@ -15,25 +16,12 @@
 use bytes::{Buf, BufMut, BytesMut};
 
 pub use {
-    t1::*, t8::*,
-    t18::*,
-    t100::*, t106::*, t107::*, t109::*,
-    t116::*,
-    t124::*, t128::*,
-    t141::*, t142::*, t144::*, t145::*, t147::*,
-    t154::*,
-    t16e::*,
-    t177::*,
-    t187::*, t188::*,
-    t191::*,
-    t511::*, t516::*,
-    t521::*, t525::*, t52d::*,
-    t545::*, t548::*,
+    t1::*, t100::*, t106::*, t107::*, t109::*, t116::*, t124::*, t128::*, t141::*, t142::*,
+    t144::*, t145::*, t147::*, t154::*, t16e::*, t177::*, t18::*, t187::*, t188::*, t191::*,
+    t511::*, t516::*, t521::*, t525::*, t52d::*, t545::*, t548::*, t8::*,
 };
 
 mod t1;
-mod t8;
-mod t18;
 mod t100;
 mod t106;
 mod t107;
@@ -49,6 +37,7 @@ mod t147;
 mod t154;
 mod t16e;
 mod t177;
+mod t18;
 mod t187;
 mod t188;
 mod t191;
@@ -59,8 +48,9 @@ mod t525;
 mod t52d;
 mod t545;
 mod t548;
+mod t8;
 
-trait TlvField: Default {
+trait TlvField {
     fn tag() -> u16;
 
     fn to_payload(&self, b: &mut BytesMut);
@@ -87,8 +77,8 @@ trait TlvField: Default {
 #[cfg(test)]
 mod tests {
     use {
-        bytes::BytesMut,
         super::{t1::TlvT1, TlvField},
+        bytes::BytesMut,
     };
 
     const T1: TlvT1 = TlvT1 { ip_ver: 1, uin: 114514 };
@@ -104,5 +94,7 @@ mod tests {
     }
 
     #[test]
-    fn to_bytes() { println!("{:?}", T1.to_bytes().to_vec()); }
+    fn to_bytes() {
+        println!("{:?}", T1.to_bytes().to_vec());
+    }
 }
