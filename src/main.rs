003876f6caebf19f2a4e07f6e4b31e6ba014cb49
千橘 雫霞 <i@qianjunakasumi.moe>
2023-06-16 20:07:46 +0900

    refactor♻️: remove dashboard static file service
    Signed-off-by: 千橘 雫霞 <i@qianjunakasumi.moe>

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ use {
     axum_server::{tls_rustls::RustlsConfig, Handle},
     std::{net::SocketAddr, time::Duration},
     tokio::time::sleep,
-    tower_http::services::{ServeDir, ServeFile},
     tracing::{info, instrument},
 };
 
@@ -58,7 +57,7 @@ async fn main() -> Result<()> {
     }
 
     Runtime::client_mut().boot().await;
-    tokio::spawn(dashboard());
+    tokio::spawn(api());
 
     info!(dsc = "うららか日和でしょでしょ～");
     Runtime::wait_stop().await;
@@ -68,7 +67,7 @@ async fn main() -> Result<()> {
 }
 
 /// 启动 Dashboard 服务
-pub async fn dashboard() {
+pub async fn api() {
     let config = RustlsConfig::from_pem_file(
         Runtime::config().dashboard.cert.clone(),
         Runtime::config().dashboard.key.clone(),
@@ -82,17 +81,12 @@ pub async fn dashboard() {
             get(|| async { "Conch 海螺 Dashboard 服务已正确运行" }),
         )
         .route("/apis", get(graphiql).post(graphql_handler))
-        .nest_service("/favicon.svg", ServeFile::new("dashboard/favicon.svg"))
         .layer(Extension(
             Schema::build(QueryRoot, MutationRoot, EmptySubscription).finish(),
-        ))
-        .nest_service(
-            "/assets",
-            ServeDir::new("dashboard/assets").not_found_service(ServeFile::new("../index.html")),
-        );
+        ));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], Runtime::config().dashboard.port));
-    info!(dsc = "Dashboard 服务启用", addr = %addr);
+    info!(dsc = "API 服务启用", addr = %addr);
 
     let h = Handle::new();
     let ha = h.clone();
@@ -106,5 +100,5 @@ pub async fn dashboard() {
         .handle(h)
         .serve(app.into_make_service())
         .await
-        .expect("启动 Dashboard 服务失败");
+        .expect("启动 API 服务失败");
 }
